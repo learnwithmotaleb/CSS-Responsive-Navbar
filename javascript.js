1f67daa6b7e54abd8095b287c295aaf01eea117a
Abdul Motaleb (Munna) <71353762+munna4@users.noreply.github.com>
2021-03-03 13:00:00 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var mybutton=document.querySelector(".toggle-button");
+var nablink=document.querySelector(".navbar-links");
+mybutton.addEventListener("click", function(){
+    nablink.classList.toggle("active");
+
+
+});
+
